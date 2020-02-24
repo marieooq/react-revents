29d0c095c0f82a19acfc1befbbe85127439e90a9
Marie Otaki <otakimarie@gmail.com>
2020-02-23 16:45:32 -0800

    end of section8-66

---
+++ src/features/event/EventDetailed/EventDetailedChat.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EventDetailedChat = () => {
+  return <div>Event Detailed Chat</div>;
+};
+
+export default EventDetailedChat;
---
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EventDetailedSidebar = () => {
+  return <div>Event Detailed Sidebar</div>;
+};
+
+export default EventDetailedSidebar;
